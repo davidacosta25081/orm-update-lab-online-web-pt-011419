ecf942caf28dbc749de59000033ffccb50574431
David Acosta <davidacosta25081@yahoo.com>
2019-04-10 01:15:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,12 +70,19 @@ end
 
 
 def self.find_by_name(name)
-  sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
-  result = DB[:conn].execute(sql,name).map |row|
-  self.new_from_db(row)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
 
-end.first
-end
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
 
 
